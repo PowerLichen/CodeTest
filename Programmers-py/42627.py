a5543079aaad20713902c7aa3d0804c0159e5d72
Powerlichen <ms.choe015@gmail.com>
2023-01-18 23:03:34 +0900

    solve p-42672

--- Programmers-py/42627.py
+++ Programmers-py/42627.py
@@ -1,34 +1,22 @@
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapify
 
 def solution(jobs):
-    answer = 0
-    jobs.sort()
+    heapify(jobs)
     n = len(jobs)
-    cur_jobs = list()
-    idx = 0
+    job_queue = list()
     time = 0
-    while True:
-        while idx < n:
-            if jobs[idx][0] > time:
-                break
-            j_time, j_value = jobs[idx]
-            heappush(cur_jobs, [j_value+time-j_time, j_time, j_value])
-            idx += 1
-        # 레디 큐에 작업이 있을 때
-        if cur_jobs:
-            nxt_job = heappop(cur_jobs)
-            time += nxt_job[2]
-            answer += time - nxt_job[1]
-        # 레디 큐에 작업이 없을 때: 디스크 대기 후 작업
-        elif idx < n:
-            nxt_job = jobs[idx]
-            time = nxt_job[0] + nxt_job[1]
-            answer += nxt_job[1] 
-        # 모든 작업이 끝났을 때
-        else:
-            break
+    answer = 0
+    while jobs or job_queue:
+        while jobs and jobs[0][0] <= time:
+            job = heappop(jobs)
+            heappush(job_queue, (job[1], job[0]))
         
+        if job_queue:
+           c_value, c_time = heappop(job_queue)
+        elif jobs:
+            c_time, c_value = heappop(jobs)
+            time = c_time
+        time += c_value
+        answer += time-c_time     
         
     return answer // n
-
-
